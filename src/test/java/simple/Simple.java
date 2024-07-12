a327e1d2ebd6b0b92eb4600e7dbce0af14c51471
Ashok Athukuri <Ashok Athukuri@LAPTOP-RLAJBEF5>
2024-07-12 20:32:45 +0530

    one

--- src/test/java/simple/Simple.java
+++ src/test/java/simple/Simple.java
@@ -4,14 +4,7 @@ public class Simple
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("hi maven");	
-
-		
-		System.out.println("Hi maven i am coming");
-		
-		System.out.println("Commited the changes");
-		
-		System.out.println("i got my first call and is from capgemini");
+		System.out.println("one");
 	}
 
 }
